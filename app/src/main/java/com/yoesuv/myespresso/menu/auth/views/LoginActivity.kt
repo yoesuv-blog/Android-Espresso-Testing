96236f565abf33cb3b3e57eddfb8355e8becb0b7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-02-25 13:31:24 +0700

    :art: layout login

--- app/src/main/java/com/yoesuv/myespresso/menu/auth/views/LoginActivity.kt
+++ app/src/main/java/com/yoesuv/myespresso/menu/auth/views/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.yoesuv.myespresso
+package com.yoesuv.myespresso.menu.auth.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.myespresso.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
