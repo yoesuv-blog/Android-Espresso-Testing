d6ddc602d08e002e6ad93024e681c7ac490b369b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-02-25 13:49:46 +0700

    :art: layout list

---
+++ app/src/main/java/com/yoesuv/myespresso/menu/list/views/ListActivity.kt
@@ -0,0 +1,12 @@
+package com.yoesuv.myespresso.menu.list.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
