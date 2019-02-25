e85ff17434b6b1cd1a43068cfe11ca1e32c60048
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-02-25 16:44:32 +0700

    :construction: create my list adapter

---
+++ app/src/main/java/com/yoesuv/myespresso/menu/list/adapters/MyListAdapter.kt
@@ -0,0 +1,36 @@
+package com.yoesuv.myespresso.menu.list.adapters
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.yoesuv.myespresso.R
+import com.yoesuv.myespresso.databinding.ItemListBinding
+import com.yoesuv.myespresso.menu.list.models.PlaceModel
+import com.yoesuv.myespresso.menu.list.viewmodels.ItemListViewModel
+
+class MyListAdapter(val myList: MutableList<PlaceModel>): RecyclerView.Adapter<MyListAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding: ItemListBinding = DataBindingUtil.inflate(inflater, R.layout.item_list, parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindView(myList[holder.adapterPosition])
+    }
+
+    class MyViewHolder(val binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindView(placeModel: PlaceModel){
+            val viewModel = ItemListViewModel(placeModel)
+            binding.itemList = viewModel
+        }
+
+    }
+}
---
+++ app/src/main/java/com/yoesuv/myespresso/menu/list/viewmodels/ItemListViewModel.kt
@@ -0,0 +1,7 @@
+package com.yoesuv.myespresso.menu.list.viewmodels
+
+import android.arch.lifecycle.ViewModel
+import com.yoesuv.myespresso.menu.list.models.PlaceModel
+
+class ItemListViewModel(placeModel: PlaceModel): ViewModel() {
+}
