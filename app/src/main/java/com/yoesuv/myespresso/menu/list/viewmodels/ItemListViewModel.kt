b8cdf6cd249d3620b2c389bc6ebde6a9f44664c8
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-02-26 08:30:01 +0700

    :pencil: show item data text

--- app/src/main/java/com/yoesuv/myespresso/menu/list/viewmodels/ItemListViewModel.kt
+++ app/src/main/java/com/yoesuv/myespresso/menu/list/viewmodels/ItemListViewModel.kt
@@ -1,7 +1,19 @@
 package com.yoesuv.myespresso.menu.list.viewmodels
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
 import com.yoesuv.myespresso.menu.list.models.PlaceModel
 
 class ItemListViewModel(placeModel: PlaceModel): ViewModel() {
+
+    var title: ObservableField<String> = ObservableField()
+    var place: ObservableField<String> = ObservableField()
+    var thumbnail: ObservableField<String> = ObservableField()
+
+    init {
+        title.set(placeModel.nama)
+        place.set(placeModel.lokasi)
+        thumbnail.set(placeModel.thumbnail)
+    }
+
 }
